40b6cd446692cea30baf1dbcc8911207d9505288
JuanMorales06 <chop052009@gmail.com>
2024-04-02 20:53:06 -0500

    add header, footer, home

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import { RegistrationFormUser } from './components/RegistrationFormUser';
 import { RegistrationFormVehicle } from './components/RegistrationFormVehicle';
 import {SelectComponent} from './components/a';
 import {LoginForm} from './components/LoginForm';
+import {Header} from './components/Header';
+import {Home} from './components/Home';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
  
   <div className="App">
+      {<Header/>}
+      {<Home/>}
+      {<Footer/>}
       {<LoginForm/>}
       {<RegistrationFormUser/>}
       {<RegistrationFormVehicle/>}
+      
+
      
 
   </div> 
