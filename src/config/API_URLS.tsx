9e14fdef2653c777277aa3ef4cbb0d46c9158315
sojoko <jonhathansojo@gmail.com>
2024-07-26 00:42:42 -0500

    add api getway uri

--- src/config/API_URLS.tsx
+++ src/config/API_URLS.tsx
@@ -1,3 +1,5 @@
 // export const API_URL = "http://127.0.0.1:8000/api/v1" 
-export const API_URL = "https://3.86.233.19:8000/api/v1" 
+// export const API_URL = "https://3.86.233.19:8000/api/v1" 
+export const API_URL = "https://713nvrio8c.execute-api.us-east-1.amazonaws.com" 
+
 
